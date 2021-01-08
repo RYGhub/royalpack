be5c24327f43212ece22387796cac81afbefd909
Stefano Pigozzi <ste.pigozzi@gmail.com>
2021-01-08 19:40:57 +0100

    💥 Implement some more commands and stuff

--- royalpack/commands/ahnonlosoio.py
+++ royalpack/commands/ahnonlosoio.py
@@ -2,11 +2,11 @@ import royalnet.engineer as engi
 
 
 @engi.PartialCommand.new(syntax="")
-def ahnonlosoio(_sentry: engi.Sentry, _msg: engi.Message, **__):
+def ahnonlosoio(*, _sentry: engi.Sentry, _msg: engi.Message, **__):
     """
     Ah, non lo so io!
     """
-    await _msg.send_reply(r"¯\_(ツ)_/¯ Ah, non lo so io!")
+    await _msg.send_reply(text=r"¯\_(ツ)_/¯ Ah, non lo so io!")
 
 
 __all__ = ("ahnonlosoio",)
