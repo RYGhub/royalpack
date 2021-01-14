0af228fbd29cafdc03363383c8124c2f62c2d7ce
Stefano Pigozzi <me@steffo.eu>
2021-01-14 17:42:37 +0100

    🔧 Remove old [i][/i] marks from the output of /color

--- royalpack/commands/color.py
+++ royalpack/commands/color.py
@@ -7,7 +7,9 @@ async def color(*, _sentry: engi.Sentry, _msg: engi.Message, **__):
     Invia un colore in chat...?
     """
     
-    await _msg.send_reply(text="[i]I am sorry, unknown error occured during working with your request, Admin were notified[/i]")
+    await _msg.send_reply(
+        text="I am sorry, unknown error occured during working with your request, Admin were notified"
+    )
 
 
 __all__ = ("color",)
